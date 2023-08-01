5b70825b38e5fe7181877976b3ebeba855389e5e
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-01 11:48:02 +0800

    style: change contact header size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -185,9 +185,9 @@ export default function Home() {
         ))}
       </section>
 
-      <section id="contact" className='bg-blue-900 text-blue-200'>
+      <section id="contact" className='bg-blue-900 text-blue-100 text-heavy'>
         <article className='min-h-1/2 max-w-[500px] mx-auto min-h-1/2 py-32 flex flex-col gap-16 px-2 sm:px-0'>
-          <h2 className='text-2xl'>Contact Me</h2>
+          <h2 className='text-4xl'>Contact Me</h2>
           <EmailForm />
         </article>
       </section>
