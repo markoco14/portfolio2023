6337f3cf29e705bfbf544efa4655deadb8603268
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-01 10:54:26 +0800

    feature: update index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,200 @@
+import EmailForm from '@/components/EmailForm';
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
+type Project = {
+  color: string;
+  heading: string;
+  description: string;
+  techStack: string;
+  image: {
+    src: string;
+    alt: string;
+  },
+  codeLink: {
+    fullstack?: string;
+    backend?: string;
+    frontend?: string;
+  };
+  siteLink: string;
+}
+
+const Project = ({ project, index }: { project: Project, index: number }) => {
+
+  return (
+    <section className={`relative h-full sm:h-1/2 ${project.color}`}>
+      <article className='grid sm:grid-cols-2 max-w-[800px] mx-auto py-32 gap-16 px-2 sm:px-0'>
+        <div className={`flex flex-col gap-8 bg-white shadow-xl rounded-xl p-8 ${index % 2 === 0  ? '' : 'sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-2'}`}>
+          <h2 className='text-2xl'>{project.heading}</h2>
+          <p>
+            {project.description}
+          </p>
+          <div>
+            {project.codeLink.frontend && (
+              <Link 
+                href={project.codeLink.frontend}
+                rel="noopener noreferrer"
+                target="_blank"
+                className='p-2 rounded'
+              >
+                Frontend
+              </Link>
+            )} 
+            {project.codeLink.backend && (
+              <Link 
+                href={project.codeLink.backend}
+                rel="noopener noreferrer"
+                target="_blank"
+                className='p-2 rounded'
+              >
+                Backend
+              </Link>
+            )}
+             {project.codeLink.fullstack && (
+              <Link 
+                href={project.codeLink.fullstack}
+                rel="noopener noreferrer"
+                target="_blank"
+                className='p-2 rounded'
+              >
+                Code
+              </Link>
+            )}
+            <Link 
+              href={project.siteLink}
+              rel="noopener noreferrer"
+              target="_blank"
+              className='p-2 rounded underline underline-offset-2 decoration-2'
+            >
+              Website
+            </Link>
+          </div>
+        </div>
+        <div className={`relative w-full aspect-square grid place-items-center rounded-xl shadow-xl`}>
+            <Image 
+              src={project.image.src}
+              alt={project.image.alt}
+              fill={true}
+              objectFit='cover'
+              className='rounded-xl'
+            />
+          </div>
+      </article>
+    </section>
+  );
+}
 
 export default function Home() {
+  const projects: Project[] = [
+    {
+      color: "",
+      heading: "Cram School Cloud",
+      description: "A school management application that lets Taiwan's local English language schools manage their student data",
+      techStack: "",
+      image: {
+        src: "/images/cramschoolcloud/cramschool1.png",
+        alt: "Cram school cloud section image",
+      },
+      codeLink: {
+        frontend: 'https://github.com/markoco14/frontend-school-mgmt',
+        backend: 'https://github.com/markoco14/student-mgmt',
+      },
+      siteLink: 'https://www.cramschoolcloud.com/'
+    },
+    {
+      color: "bg-gray-500",
+      heading: "Hao Hao Sound",
+      description: " A sound library application that lets users listen to short sounds perfect for any situation.",
+      techStack: "",
+      image: {
+        src: "/images/haohaosound/haohao1.png",
+        alt: "Hao Hao Sound hero section image",
+      },
+      codeLink: {
+        fullstack: 'https://github.com/markoco14/haohaosound',
+      },
+      siteLink: 'https://www.haohaosound.com/'
+    },
+    {
+      color: "bg-green-500",
+      heading: "Expensave",
+      description: "An expense tracket application that lets users track their income, budgets, and daily spending.",
+      techStack: "",
+      image: {
+        src: "/images/expensave/expensave1.png",
+        alt: "Expensave hero section image",
+      },
+      codeLink: {
+        fullstack: 'https://github.com/markoco14/expense-tracker',
+      },
+      siteLink: 'https://mymoneybags.herokuapp.com/'
+    },
+    {
+      color: "",
+      heading: "Scriptt",
+      description: "A dialogue writer application that lets English teachers create dialgoues for their English classes.",
+      techStack: "",
+      image: {
+        src: "/images/scriptt/scriptt2.png",
+        alt: "Scriptt hero section image",
+      },
+      
+      codeLink: {
+        fullstack: 'https://github.com/markoco14/dialogue-writer-app'
+      },
+      siteLink: 'https://markoco14.github.io/dialogue-writer-app/'
+    },
+    {
+      color: "bg-slate-700",
+      heading: "Taiwan Income Tax Calculator",
+      description: "A tax calculator that lets foreigners check their taxes and income when they move to Taiwan.",
+      techStack: "",
+      image: {
+        src: "/images/tiss/tiss3.png",
+        alt: "Tiss hero section image",
+      },
+      codeLink: {
+        fullstack: 'https://github.com/markoco14/tiss'
+      },
+      siteLink: 'https://markoco14.github.io/tiss/'
+    }
+  ];
+  
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+    <main>
+      <nav className="z-10 text-white px-2 bg-black/50 flex justify-end items-center gap-4 w-full fixed top-0 left-0 h-[48px]">
+        <Link href="#home">Home</Link>
+        <Link href="#projects">Projects</Link>
+        <Link href="#contact">Contact</Link>
+      </nav>
+      <section 
+        id="home"
+        className='h-screen bg-blue-600 text-gray-100 grid place-items-center pt-[48px]'
+      >
+        <article className='max-w-[800px] mx-auto min-h-screen py-32 flex flex-col gap-16 px-2 sm:px-0'>
+          <h1 className='text-4xl'>Find problems. <br></br>Build solutions. <br></br>Live better.</h1>
+          <p>I&apos;m Mark O&apos;Connor. I&apos;m a Software Engineer located in Taipei, Taiwan. 
+            I use Python and JavaScript to build things people need.
           </p>
-        </a>
+        </article>
+      </section>
+      <section id="projects">
+        {projects.map((project: Project, index: number) => (
+          <Project key={index} index={index} project={project}/>
+        ))}
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section id="contact" className='bg-blue-900 text-blue-200'>
+        <article className='h-screen max-w-[800px] mx-auto min-h-1/2 py-32 flex flex-col gap-16 px-2 sm:px-0'>
+          <h2 className='text-2xl'>Contact Me</h2>
+          <EmailForm />
+        </article>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer>
+        <p>this is the footer</p>
+      </footer>
     </main>
   )
 }
