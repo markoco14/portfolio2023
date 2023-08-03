c4631a69a54ea87966bd57295f8ce8daf49659b2
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-03 16:29:14 +0800

    feature: add Head tag and title to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,15 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
+import Head from "next/head";
 
 export default function Home() {
 
   return (
     <main>
+      <Head>
+        <title>Mark O&apos;Connor | Full-Stack Developer & Solutions Architect</title>
+      </Head>
       <Navbar />
       <Hero />
       <Projects />
