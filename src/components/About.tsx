f6e833cd4284f10e28f5a1c5416ef41494f116c6
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-04 18:31:26 +0800

    feature: update about section description

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,7 @@ export default function About() {
 			<div className="bg-slate-900 text-white min-h-[75vh] grid place-items-center pt-[72px] pb-24">
 				<div className="max-w-[600px] mb-24 flex flex-col gap-8 px-4 sm:px-0">
 					<p className="text-3xl">Hi, I&apos;m Mark. Nice to meet you.</p>
-					<p className="text-lg">I was drawn to programming by dreams of what I could create.
+					<p className="text-lg">I was drawn to programming by dreams of the things I could create.
 						I&apos;m passionate about turning ideas into reality. 
 						I love writing clean code but the real joy is 
 						creating beautiful products that people love using.
