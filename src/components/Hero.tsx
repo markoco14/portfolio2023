122fcda838c0ddfb42f0c343b9f741acb9261f9e
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-04 17:43:22 +0800

    feature: style hero and add photo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,29 @@
+import Image from "next/image"
+
 export default function Hero() {
  
   return (
 		<section
         id="home"
-        className="h-screen sm:min-h-[80vh] text-gray-100 bg-gradient-to-r from-blue-500 to-blue-600 sm:via-blue-600 sm:to-blue-500 grid place-items-center pt-[48px]"
+        className="relative min-h-screen sm:min-h-[80vh] text-gray-100 bg-gradient-to-r from-blue-600 sm:from-blue-800 to-blue-800 sm:via-blue-600 sm:to-blue-800 grid place-items-center pt-[48px]"
       >
-        <article className="max-w-[800px] mx-auto min-h-[screen] py-24 px-2 sm:px-0">
-          <p className="text-5xl font-bold mb-8">Full-stack Developer</p>
-          <h1 className="text-2xl mb-16">
-            I take problems and build solutions that help others live better.
-          </h1>
+        <article className="grid sm:grid-cols-2 items-center py-12 max-w-[800px] mx-auto min-h-screen px-2 sm:px-0">
+          <div className="mb-8">
+            <p className="text-3xl text-left mb-4">Mark O&apos;Connor</p>
+            <p className="text-5xl font-bold mb-4">Full Stack Developer</p>
+            <h1 className="text-2xl">
+              I find problems and build solutions that help others live better.
+            </h1>
+          </div>
+          <div className="mx-auto">
+            <Image 
+              src="/images/profiles/me_cropped.jpg"
+              alt="A profile photo of Mark. The developer of the site."
+              width={250}
+              height={250}
+              className="rounded-full"
+            />
+          </div>
         </article>
       </section>
   )
