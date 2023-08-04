15bbed3154ed50567548be5826bbd5478913c743
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-04 14:46:19 +0800

    feature: adjust text in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,17 +3,13 @@ export default function Hero() {
   return (
 		<section
         id="home"
-        className="h-screen text-gray-100 bg-gradient-to-r from-blue-500 to-blue-600 sm:via-blue-600 sm:to-blue-500 grid place-items-center pt-[48px]"
+        className="h-screen sm:min-h-[80vh] text-gray-100 bg-gradient-to-r from-blue-500 to-blue-600 sm:via-blue-600 sm:to-blue-500 grid place-items-center pt-[48px]"
       >
-        <article className="max-w-[800px] mx-auto min-h-screen py-24 flex flex-col gap-16 px-2 sm:px-0">
-          <h1 className="text-4xl">
-            Find problems. <br></br>Build solutions. <br></br>Live better.
+        <article className="max-w-[800px] mx-auto min-h-[screen] py-24 px-2 sm:px-0">
+          <p className="text-5xl font-bold mb-8">Full-stack Developer</p>
+          <h1 className="text-2xl mb-16">
+            I take problems and build solutions that help others live better.
           </h1>
-          <p>
-            I&apos;m Mark O&apos;Connor. I&apos;m a Software Engineer located in
-            Taipei, Taiwan. I use Python and JavaScript to build things people
-            need.
-          </p>
         </article>
       </section>
   )
