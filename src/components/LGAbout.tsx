f4b703c81e5410f729f64ce58eacad244510e62e
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-04 18:39:10 +0800

    feature: improve about section styles

--- src/components/LGAbout.tsx
+++ src/components/LGAbout.tsx
@@ -3,47 +3,59 @@ export default function LGAbout() {
 	return (
 		<>
 			<article className="border-r-2 hidden sm:flex sm:flex-col sm:items-center sm:mb-0 px-1">
-				<h3 className="text-blue-700 text-xl sm:text-center mb-2">
-					Frontend Developer
-				</h3>
-				<p className="mb-4 sm:text-center">I like to make complex interactions look and feel simple.</p>
-				<h4 className="text-blue-700 mb-2">Languages</h4>
-				<p className="whitespace-normal sm:text-center mb-4">HTML | CSS | JavaScript</p>
-				<h4 className="text-blue-700 mb-2">Tools</h4>
-				<ul className="flex flex-col gap-1">
-					<li>ReactJS</li>
-					<li>NextJS</li>
-					<li>MirageJS</li>
-					<li>AntD</li>
-					<li>AG Grid</li>
-					<li>Tailwind CSS</li>
-					<li>Bootstrap</li>
-					<li>Material UI</li>
-					<li>Headless UI</li>
-				</ul>
+				<div className="mb-4 text-center">
+					<h3 className="text-blue-700 text-xl sm:text-center mb-2">
+						Frontend Developer
+					</h3>
+					<p className="sm:text-center">I like to make complex interactions look and feel simple.</p>
+				</div>
+				<div className="mb-4 text-center">
+					<h4 className="text-blue-700 mb-2">Languages</h4>
+					<p className="whitespace-normal sm:text-center">HTML | CSS | JavaScript</p>
+				</div>
+				<div className="mb-4 text-center">
+					<h4 className="text-blue-700 mb-2">Tools</h4>
+					<ul className="flex flex-col gap-1">
+						<li>ReactJS</li>
+						<li>NextJS</li>
+						<li>MirageJS</li>
+						<li>AntD</li>
+						<li>AG Grid</li>
+						<li>Tailwind CSS</li>
+						<li>Bootstrap</li>
+						<li>Material UI</li>
+						<li>Headless UI</li>
+					</ul>
+				</div>
 			</article>
 			<article className="hidden sm:flex flex-col sm:items-center mb-4 sm:mb-0 px-1">
-				<h3 className="text-blue-700 text-xl sm:text-center mb-2">
-					Backend Developer
-				</h3>
-				<p className="mb-4 sm:text-center">I like to dream up database schemas and improve performance.</p>
-				<h4 className="text-blue-700 mb-2">Languages</h4>
-				<p className="whitespace-normal sm:text-center mb-4">PHP | Python | MySQL | PostgreSQL | NoSQL</p>
-				<h4 className="text-blue-700 mb-2">Tools</h4>
-				<ul className="flex flex-col gap-1">
-					<li>Git</li>
-					<li>AWS</li>
-					<li>GCP</li>
-					<li>Cloud Functions</li>
-					<li>Laravel</li>
-					<li>Django</li>
-					<li>NodeJS</li>
-					<li>Docker</li>
-					<li>Planet Scale</li>
-					<li>Firebase</li>
-					<li>Supabase</li>
-					<li>Postman</li>
-				</ul>
+				<div className="mb-4 text-center">
+					<h3 className="text-blue-700 text-xl sm:text-center mb-2">
+						Backend Developer
+					</h3>
+					<p className="sm:text-center">I like to dream up database schemas and improve performance.</p>
+				</div>
+				<div className="mb-4 text-center">
+					<h4 className="text-blue-700 mb-2">Languages</h4>
+					<p className="whitespace-normal sm:text-center">PHP | Python | MySQL | PostgreSQL | NoSQL</p>
+				</div>
+				<div className="mb-4 text-center">
+					<h4 className="text-blue-700 mb-2">Tools</h4>
+					<ul className="flex flex-col gap-1">
+						<li>Git</li>
+						<li>AWS</li>
+						<li>GCP</li>
+						<li>Cloud Functions</li>
+						<li>Laravel</li>
+						<li>Django</li>
+						<li>NodeJS</li>
+						<li>Docker</li>
+						<li>Planet Scale</li>
+						<li>Firebase</li>
+						<li>Supabase</li>
+						<li>Postman</li>
+					</ul>
+				</div>
 			</article>
 		</>
 	);
--- src/components/MobileAbout.tsx
+++ src/components/MobileAbout.tsx
@@ -15,53 +15,65 @@ export default function MobileAbout() {
 				</div>
 				{isFrontend ? (
 					<article className="sm:hidden">
-						
-					<h3 className="text-blue-700 text-xl sm:text-center mb-2">
-						Frontend Developer
-					</h3>
-					<p className="mb-6">I like to make complex interactions look and feel simple.</p>
-					<h4 className="text-blue-700 mb-2">Languages</h4>
-					<p className="whitespace-normal sm:text-center mb-4">HTML | CSS | JavaScript</p>
-					<h4 className="text-blue-700 mb-2">Tools</h4>
-					<ul className="grid grid-cols-2 gap-1">
-						<li>ReactJS</li>
-						<li>NextJS</li>
-						<li>MirageJS</li>
-						<li>AntD</li>
-						<li>AG Grid</li>
-						<li>Tailwind CSS</li>
-						<li>Bootstrap</li>
-						<li>Material UI</li>
-						<li>Headless UI</li>
-						<li>Canva</li>
-						<li>Coolors</li>
-						<li>Figma</li>
-					</ul>
+					
+					<div className="mb-4">
+						<h3 className="text-blue-700 text-xl sm:text-center mb-2">
+							Frontend Developer
+						</h3>
+						<p >I like to make complex interactions look and feel simple.</p>
+					</div>
+					<div className="mb-4">
+						<h4 className="text-blue-700 text-lg mb-2">Languages</h4>
+						<p className="whitespace-normal sm:text-center">HTML | CSS | JavaScript</p>
+					</div>
+					<div className="mb-4">
+
+						<h4 className="text-blue-700 text-lg mb-2">Tools</h4>
+						<ul className="grid grid-cols-2 gap-1">
+							<li>ReactJS</li>
+							<li>NextJS</li>
+							<li>MirageJS</li>
+							<li>AntD</li>
+							<li>AG Grid</li>
+							<li>Tailwind CSS</li>
+							<li>Bootstrap</li>
+							<li>Material UI</li>
+							<li>Headless UI</li>
+							<li>Canva</li>
+							<li>Coolors</li>
+							<li>Figma</li>
+						</ul>
+					</div>
 				</article>
 				) : (
 					<article className="sm:hidden">
-					
-					<h3 className="text-blue-700 text-xl sm:text-center mb-2">
-						Backend Developer
-					</h3>
-					<p className="mb-6">I like to dream up database schemas and improve performance.</p>
-					<h4 className="text-blue-700 mb-2">Languages</h4>
-					<p className="whitespace-normal sm:text-center mb-4">PHP | Python | MySQL | PostgreSQL | NoSQL</p>
-					<h4 className="text-blue-700 mb-2">Tools</h4>
-					<ul className="grid grid-cols-2 gap-1">
-						<li>Git</li>
-						<li>AWS EC2</li>
-						<li>GCP</li>
-						<li>Cloud Functions</li>
-						<li>Laravel</li>
-						<li>Django</li>
-						<li>NodeJS</li>
-						<li>Docker</li>
-						<li>Planet Scale</li>
-						<li>Firebase</li>
-						<li>Supabase</li>
-						<li>Postman</li>
-					</ul>
+					<div className="mb-4">
+						<h3 className="text-blue-700 text-xl sm:text-center mb-2">
+							Backend Developer
+						</h3>
+						<p>I like to dream up database schemas and improve performance.</p>
+					</div>
+					<div className="mb-4">
+						<h4 className="text-blue-700 mb-2 text-lg">Languages</h4>
+						<p className="whitespace-normal sm:text-center">PHP | Python | MySQL | PostgreSQL | NoSQL</p>
+					</div>
+					<div className="mb-4">
+						<h4 className="text-blue-700 mb-2 text-lg">Tools</h4>
+						<ul className="grid grid-cols-2 gap-1">
+							<li>Git</li>
+							<li>AWS EC2</li>
+							<li>GCP</li>
+							<li>Cloud Functions</li>
+							<li>Laravel</li>
+							<li>Django</li>
+							<li>NodeJS</li>
+							<li>Docker</li>
+							<li>Planet Scale</li>
+							<li>Firebase</li>
+							<li>Supabase</li>
+							<li>Postman</li>
+						</ul>
+					</div>
 				</article>
 				)}				
 			</>
