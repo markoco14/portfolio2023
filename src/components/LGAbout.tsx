b5d281810f6472265f9ea3d586e5476c7630dced
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-04 11:42:44 +0800

    feature: create mobile and desktop components for about section details

---
+++ src/components/LGAbout.tsx
@@ -0,0 +1,50 @@
+export default function LGAbout() {
+
+	return (
+		<>
+			<article className="hidden sm:flex flex-col sm:items-center mb-4 sm:mb-0">
+				<h3 className="text-blue-700 text-xl sm:text-center mb-2">
+					Frontend Developer
+				</h3>
+				<p className="mb-4">I like to make complex interactions look and feel simple.</p>
+				<h4 className="text-blue-700 mb-2">Languages I speak:</h4>
+				<p className="whitespace-normal sm:text-center mb-4">HTML | CSS | JavaScript</p>
+				<h4 className="text-blue-700 mb-2">Tools of the Trade:</h4>
+				<ul className="flex flex-col gap-1">
+					<li>ReactJS</li>
+					<li>NextJS</li>
+					<li>MirageJS</li>
+					<li>AntD</li>
+					<li>AG Grid</li>
+					<li>Tailwind CSS</li>
+					<li>Bootstrap</li>
+					<li>Material UI</li>
+					<li>Headless UI</li>
+				</ul>
+			</article>
+			<article className="hidden sm:flex flex-col sm:items-center mb-4 sm:mb-0">
+				<h3 className="text-blue-700 text-xl sm:text-center mb-2">
+					Backend Developer
+				</h3>
+				<p className="mb-4">I like to dream up database schemas and improve performance.</p>
+				<h4 className="text-blue-700 mb-2">Languages I speak:</h4>
+				<p className="whitespace-normal sm:text-center mb-4">PHP | Python | MySQL | PostgreSQL | NoSQL</p>
+				<h4 className="text-blue-700 mb-2">Tools of the Trade:</h4>
+				<ul className="flex flex-col gap-1">
+					<li>Git</li>
+					<li>AWS</li>
+					<li>GCP</li>
+					<li>Cloud Functions</li>
+					<li>Laravel</li>
+					<li>Django</li>
+					<li>NodeJS</li>
+					<li>Docker</li>
+					<li>Planet Scale</li>
+					<li>Firebase</li>
+					<li>Supabase</li>
+					<li>Postman</li>
+				</ul>
+			</article>
+		</>
+	);
+}
---
+++ src/components/MobileAbout.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+export default function MobileAbout() {
+	const [isFrontend, setIsFrontend] = useState<boolean>(true);
+ 
+  return (
+			<>
+				<div className="sm:hidden mb-4 flex justify-start gap-4">
+					<button 
+					className={`${isFrontend ? 'underline underline-offset-2 decoration-2 decoration-blue-700' : ''}`}
+					onClick={() => setIsFrontend(true)}>Frontend</button>
+					<button 
+					className={`${isFrontend ? '' : 'underline underline-offset-2 decoration-2 decoration-blue-700'}`}
+					onClick={() => setIsFrontend(false)}>Backend</button>
+				</div>
+				{isFrontend ? (
+					<article className="sm:hidden">
+						
+					<h3 className="text-blue-700 text-xl sm:text-center mb-2">
+						Frontend Developer
+					</h3>
+					<p className="mb-6">I like to make complex interactions look and feel simple.</p>
+					<h4 className="text-blue-700 mb-2">Languages I speak:</h4>
+					<p className="whitespace-normal sm:text-center mb-4">HTML | CSS | JavaScript</p>
+					<h4 className="text-blue-700 mb-2">Tools of the Trade:</h4>
+					<ul className="grid grid-cols-2 gap-1">
+						<li>ReactJS</li>
+						<li>NextJS</li>
+						<li>MirageJS</li>
+						<li>AntD</li>
+						<li>AG Grid</li>
+						<li>Tailwind CSS</li>
+						<li>Bootstrap</li>
+						<li>Material UI</li>
+						<li>Headless UI</li>
+						<li>Canva</li>
+						<li>Coolors</li>
+						<li>Figma</li>
+					</ul>
+				</article>
+				) : (
+					<article className="sm:hidden">
+					
+					<h3 className="text-blue-700 text-xl sm:text-center mb-2">
+						Backend Developer
+					</h3>
+					<p className="mb-6">I like to dream up database schemas and improve performance.</p>
+					<h4 className="text-blue-700 mb-2">Languages I speak:</h4>
+					<p className="whitespace-normal sm:text-center mb-4">PHP | Python | MySQL | PostgreSQL | NoSQL</p>
+					<h4 className="text-blue-700 mb-2">Tools of the Trade:</h4>
+					<ul className="grid grid-cols-2 gap-1">
+						<li>Git</li>
+						<li>AWS EC2</li>
+						<li>GCP</li>
+						<li>Cloud Functions</li>
+						<li>Laravel</li>
+						<li>Django</li>
+						<li>NodeJS</li>
+						<li>Docker</li>
+						<li>Planet Scale</li>
+						<li>Firebase</li>
+						<li>Supabase</li>
+						<li>Postman</li>
+					</ul>
+				</article>
+				)}				
+			</>
+  )
+}
