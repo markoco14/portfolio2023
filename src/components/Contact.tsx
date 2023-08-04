1b27108e771cc9f206002e8dabdc9187855170a6
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-04 19:02:03 +0800

    feature: update all svg for react formatting

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
         id="contact"
         className="mix-h-screen text-blue-100 bg-gradient-to-r from-blue-500 to-blue-600 sm:via-blue-600 sm:to-blue-500"
       >
-        <article className="min-h-1/2 max-w-[500px] mx-auto min-h-screen py-24 flex flex-col gap-16 px-2 sm:px-0">
+        <article className="min-h-1/2 max-w-[500px] mx-auto py-24 flex flex-col gap-16 px-2 sm:px-0">
           <h2 className="text-5xl font-bold">Contact Me</h2>
           <EmailForm />
         </article>
