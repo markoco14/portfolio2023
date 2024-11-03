99d7a7cdf6cc586ee262d8b67a9e828c3c213972
Mark <mark.oconnor14@gmail.com>
2024-11-03 20:59:24 +0800

    initialize ESL section

---
+++ src/components/ESL.tsx
@@ -0,0 +1,14 @@
+export default function ESL () {
+    return (
+        <section
+            id="esl"
+            className="px-2 sm:px-4 py-24 min-h-screen max-w-[1000px] mx-auto"
+            >
+            <h2 className="text-5xl text-bold mb-8">ESL</h2>
+            <p className="max-w-[65ch]">
+                I&apos;ve decided to automate some tasks at work and create apps to help me make materials for my 
+                students. This section is dedicated to the tools that make my life easier as a teacher.
+            </p>
+        </section>
+    )
+}
